5c1d208afa7b356f7a9c7967fccf742193192544
Ronnie Feng <ronnie@ronniefeng.com>
2021-01-30 19:49:55 -0500

    initialization

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>hi</div>
+  <Ronnie/>
+</template>
+
+<script>
+import Ronnie from "./components/Ronnie.vue";
+
+export default {
+  name: 'App',
+  components: {
+    Ronnie
+  }
+}
+</script>
---
+++ src/components/Ronnie.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        what the fuck
+    </div>
+</template>
