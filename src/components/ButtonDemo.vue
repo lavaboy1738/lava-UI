6039ee974b50bba593f40b90eca2b088fccc8342
Ronnie Feng <ronnie@ronniefeng.com>
2021-02-03 15:45:40 -0500

    initialize button component

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,5 +1,12 @@
 <template>
     <div>
-        Button Demo
+        <Button>What up<Button/>
     </div>
 </template>
+
+<script lang="ts">
+import Button from "../library/Button.vue";
+export default {
+    components: {Button}
+}
+</script>
