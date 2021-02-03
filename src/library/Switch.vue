c070fa3131677eb208c51c20b14dee4e5852320f
Ronnie Feng <ronnie@ronniefeng.com>
2021-02-02 20:48:07 -0500

    switch toggle complete

--- src/library/Switch.vue
+++ src/library/Switch.vue
@@ -1,6 +1,19 @@
 <template>
-    <button> <span></span> </button>
+    <button :class="{checked: checked}" @click="toggle" > <span></span> </button>
 </template>
+
+<script lang="ts">
+import { ref } from 'vue'
+export default {
+    setup(){
+        const checked = ref(false);
+        const toggle = () => {
+            checked.value = !checked.value
+        }
+        return {checked, toggle}
+    }
+}
+</script>
 
 <style lang="scss" scoped>
     $height: 1.5rem;
@@ -8,18 +21,28 @@
     button{
         height: $height;
         width: $height * 2;
+        outline: none;
         border: none;
-        background: blue;
+        background: grey;
         border-radius: $height/2;
         position: relative;
-    }
-    span{
-        position: absolute;
-        top: 0.1rem;
-        left: 0.1rem;
-        height: $height2;
-        width: $height2;
-        background: white;
-        border-radius: $height2 / 2;
+        cursor: pointer;
+        span{
+            position: absolute;
+            top: 0.1rem;
+            left: 0.1rem;
+            height: $height2;
+            width: $height2;
+            background: white;
+            border-radius: $height2 / 2;
+            transition: 0.3s all ease;
+        }
+        &.checked{
+            background: blue;
+            span{
+                transition: 0.3s all ease;
+                left: calc(100% - #{$height2} - 0.1rem);
+            }
+        }
     }
 </style>
