23f2a60b68a7043153cd93dea0657f83efaeab03
Ronnie Feng <ronnie@ronniefeng.com>
2021-02-03 19:43:37 -0500

    adding events and dynamic class

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,12 +1,25 @@
 <template>
     <div>
-        <Button>What up<Button/>
+        <Button @click="onClick" @mouseover="onMouseOver" theme="button" >Button</Button>
+        <Button @click="onClick" @mouseover="onMouseOver" >Default</Button>
+        <Button @click="onClick" @mouseover="onMouseOver" theme="link" >Link</Button>
+        <Button @click="onClick" @mouseover="onMouseOver" theme="text" >Text</Button>
     </div>
 </template>
 
 <script lang="ts">
 import Button from "../library/Button.vue";
 export default {
-    components: {Button}
+    components: {Button},
+    setup(){
+        const onClick = ()=>{
+
+        }
+        const onMouseOver = ()=>{
+
+        }
+
+        return{onClick, onMouseOver}
+    }
 }
 </script>
--- src/library/Button.vue
+++ src/library/Button.vue
@@ -1,5 +1,16 @@
 <template>
-    <button>
+    <button v-bind="$attrs" class="lava-button" :class="`theme-${theme}`">
         <slot/>
     </button>
 </template>
+
+<script lang="ts">
+export default {
+    props: {
+        theme: {
+            type: String,
+            default: "button"
+        },
+    }
+}
+</script>
