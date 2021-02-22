0735a461fab995046237483a3e91f29c185bda30
Ronnie Feng <ronnie@ronniefeng.com>
2021-02-21 19:49:29 -0500

    implement type checking for Tabs's children types

--- src/library/Tabs.vue
+++ src/library/Tabs.vue
@@ -3,3 +3,18 @@
         Tabs Component
     </div>
 </template>
+
+<script lang="ts">
+import Tab from "./Tab.vue";
+export default {
+    setup(props, context){
+        //checking the type of Tabs's children and make sure they're all the Tab type. 
+        const defaults = context.slots.default();
+        defaults.forEach((tag)=>{
+            if(tag.type !== Tab){
+                throw new Error("Tabs's children must be Tab")
+            }
+        })
+    }
+}
+</script>
