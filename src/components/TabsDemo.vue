413fbf5c97bc5429710fdbfa9d61cfa2d89d9f41
Ronnie Feng <ronnie@ronniefeng.com>
2021-02-27 20:27:42 -0500

    adding tab selection and content switching

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,6 +1,6 @@
 <template>
     <h1>Tabs Demo</h1>
-    <Tabs>
+    <Tabs v-model:selected="defaultTab">
         <Tab title="Tab1">Tab 1 Content</Tab>
         <Tab title="Tab2">Tab 2 Content</Tab>
     </Tabs>
@@ -9,7 +9,14 @@
 <script lang="ts">
 import Tabs from "../library/Tabs.vue";
 import Tab from "../library/Tab.vue";
+import { ref } from 'vue';
 export default {
-    components: {Tabs, Tab}
+    components: {Tabs, Tab},
+    setup(){
+        const defaultTab = ref("Tab1");
+        return{
+            defaultTab
+        }
+    }
 }
 </script>
--- src/library/Tabs.vue
+++ src/library/Tabs.vue
@@ -1,10 +1,17 @@
 <template>
     <div class="lava-tabs">
         <div class="lava-tabs-nav">
-            <div class="lava-tabs-nav-item"  v-for="(title, index) in titles" :key="index">{{title}}</div>
+            <div class="lava-tabs-nav-item" 
+            v-for="(title, index) in titles" :key="index"
+            :class="{selected: title === selected}"
+            @click="selectTab(title)"
+             >{{title}}</div>
         </div>
         <div class="lava-tabs-content">
-            <component class="lava-tabs-content-item" v-for="(component, index) in defaults" :key="index" :is="component"/>
+            <component class="lava-tabs-content-item" v-for="(component, index) in defaults" 
+            :key="index" :is="component"
+            :class="{selected: component.props.title === selected}"
+            />
         </div>
     </div>
 </template>
@@ -12,6 +19,11 @@
 <script lang="ts">
 import Tab from "./Tab.vue";
 export default {
+    props:{
+        selected: {
+            type: String,
+        }
+    },
     setup(props, context){
         //checking the type of Tabs's children and make sure they're all the Tab type. 
         const defaults = context.slots.default();
@@ -20,12 +32,18 @@ export default {
                 throw new Error("Tabs's children must be Tab")
             }
         })
+
         const titles = defaults.map((tag)=>{
             return tag.props.title
         })
+
+        const selectTab = (title: string)=>{
+            context.emit("update:selected", title)
+        }
         return {
             defaults,
-            titles
+            titles,
+            selectTab
         }
     }
 }
@@ -54,6 +72,12 @@ export default {
         }
         &-content {
             padding: 8px 0;
+            &-item{
+                display: none;
+                &.selected{
+                    display: block;
+                }
+            }
         }
     }
 </style>
