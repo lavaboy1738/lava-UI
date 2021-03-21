5c2a1e3dbbd617a92128bb7ceff13b6ac8a0317e
Ronnie Feng <ronnie@ronniefeng.com>
2021-03-20 21:27:37 -0400

    adding prism to get started page

--- src/views/GetStarted.vue
+++ src/views/GetStarted.vue
@@ -1,33 +1,40 @@
 <template>
-    <article class="markdown-body">
-        <h1>Get Started</h1>
-        <h2>Import Components</h2>
-            <pre><code>import {Button} from "lava-ui";<br>import "lava-ui/dist/index.css";</code></pre>
-        <h2>Register Components</h2>
-            <pre><code>export default {
-  name: "ComponentName",
-  components: {
-    "lava-button": Button,
-  },
-}</code></pre>
-        <h2>Usage</h2>
-        <p>code</p>
-        <pre><code>&lt;template&gt;
-  &lt;div&gt;
-    &lt;lava-button&gt;默认按钮&lt;/lava-button&gt;
-  &lt;/div&gt;
-&lt;/template&gt;
-</code></pre>
-        <p>example</p>
-        <Button>Default Button</Button>
-    </article>
+    <h1>Get Started</h1>
+    <h2>Import Components</h2>
+        <pre class="language-javascript" v-html="Prism.highlight(importCode, Prism.languages.javascript, 'javascript')"></pre>
+    <h2>Register Components</h2>
+        <pre class="language-javascript" v-html="Prism.highlight(registerCode, Prism.languages.javascript, 'javascript')"></pre>
+    <h2>Usage</h2>
+        <pre class="language-html" v-html="Prism.highlight(usageCode, Prism.languages.html, 'html')"></pre>
+    <h3>Example</h3>
+    <Button>Default Button</Button>
 </template>
 
-<script>
+<script lang="ts">
+import "prismjs";
+import "prismjs/themes/prism-okaidia.css";
 import Button from "../library/Button.vue";
+const Prism = (window as any).Prism;
+
+const importCode = `import {Button} from \"lava-ui\";\r\nimport \"lava-ui\/dist\/index.css\";`;
+const registerCode = `export default {\r\n  name: \"ComponentName\",\r\n  components: {\r\n    \"lava-button\": Button,\r\n  },\r\n}`;
+const usageCode = `<template>\r\n  <div>\r\n    <lava-button>Default Button<\/lava-button>\r\n  <\/div>\r\n<\/template>`;
+
 export default {
     components:{
         Button,
+    },
+    setup(){
+        return{Prism, importCode, registerCode, usageCode}
     }
 }
 </script>
+
+<style lang="scss" scoped>
+    h2{
+        margin-top: 2rem;
+    }
+    pre{
+        margin-bottom: 2rem;
+    }
+</style>
