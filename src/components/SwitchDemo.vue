694b09f236e2501aaf405ba3126c62b5365ad6fd
Ronnie Feng <ronnie@ronniefeng.com>
2021-03-20 20:57:42 -0400

    enable hiding code

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -6,9 +6,9 @@
         <Switch v-model:value="bool" />
     </div>
     <div class="demo-actions">
-        <Button>Source Code</Button>
+        <Button @click="codeVisible1 = !codeVisible1">Source Code</Button>
     </div>
-    <div class="demo-code">
+    <div class="demo-code" v-if="codeVisible1">
         <pre class="language-html" v-html="Prism.highlight(textCode1, Prism.languages.html, 'html')"></pre>
     </div>
 </div>
@@ -18,9 +18,9 @@
         <Switch disabled/>
     </div>
     <div class="demo-actions">
-        <Button>Source Code</Button>
+        <Button @click="codeVisible2 = !codeVisible2" >Source Code</Button>
     </div>
-    <div class="demo-code">
+    <div class="demo-code" v-if="codeVisible2">
         <pre class="language-html" v-html="Prism.highlight(textCode2, Prism.languages.html, 'html')"></pre>
     </div>
 </div>
@@ -34,14 +34,16 @@ import Button from "../library/Button.vue";
 import { ref } from 'vue';
 
 const Prism = (window as any).Prism;
-const textCode1 = `<template>\r\n    <Switch v-model:value=\"bool\" \/>\r\n<\/template>\r\n\r\n<script lang=\"ts\">\r\nimport { ref } from 'vue';\r\nimport Switch from \"..\/library\/Switch.vue\";\r\nexport default {\r\n     components: {Switch},\r\n    setup(){\r\n        const bool = ref(false);\r\n        return{bool};\r\n    }\r\n}\r\n<\/script>`;
-const textCode2 = `<template>\r\n    <Switch v-model:value=\"bool\" disabled \/>\r\n<\/template>\r\n\r\n<script lang=\"ts\">\r\nimport { ref } from 'vue';\r\nimport Switch from \"..\/library\/Switch.vue\";\r\nexport default {\r\n     components: {Switch},\r\n    setup(){\r\n        const bool = ref(false);\r\n        return{bool};\r\n    }\r\n}\r\n<\/script>`;
+const textCode1 = `<template>\r\n    <Switch v-model:value=\"bool\" \/>\r\n<\/template>\r\n\r\n<script lang=\"ts\">\r\nimport { ref } from 'vue';\r\nimport Switch from \"..\/library\/Switch.vue\";\r\nexport default {\r\n    components: {Switch},\r\n    setup(){\r\n        const bool = ref(false);\r\n        return{bool};\r\n    }\r\n}\r\n<\/script>`;
+const textCode2 = `<template>\r\n    <Switch v-model:value=\"bool\" disabled \/>\r\n<\/template>\r\n\r\n<script lang=\"ts\">\r\nimport { ref } from 'vue';\r\nimport Switch from \"..\/library\/Switch.vue\";\r\nexport default {\r\n    components: {Switch},\r\n    setup(){\r\n        const bool = ref(false);\r\n        return{bool};\r\n    }\r\n}\r\n<\/script>`;
 
 export default {
     components: {Switch, Button},
     setup(){
         const bool = ref(false);
-        return{Prism, textCode1 ,textCode2, bool}
+        const codeVisible1 = ref(false);
+        const codeVisible2 = ref(false);
+        return{Prism, textCode1 ,textCode2, bool, codeVisible1, codeVisible2}
     }
 }
 </script>
