4f18fac2ba10549a1e3cfd661920f96afe166d30
Ronnie Feng <ronnie@ronniefeng.com>
2021-03-21 21:47:24 -0400

    tabs demo complete

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,21 +1,27 @@
 <template>
     <h1>Tabs Demo</h1>
-    <Tabs v-model:selected="defaultTab">
-        <Tab title="Tab1">Tab 1 Content</Tab>
-        <Tab title="Tab2">Tab 2 Content</Tab>
-    </Tabs>
+    <ComponentDemo title="Multi-Tabs" v-bind:sourceCode="tabsCode">
+        <Tabs v-model:selected="defaultTab">
+            <Tab title="Tab1">My name is Jeff</Tab>
+            <Tab title="Tab2">Jeff</Tab>
+            <Tab title="Tab3">Hello Jeff</Tab>
+        </Tabs>
+    </ComponentDemo>
 </template>
 
 <script lang="ts">
 import Tabs from "../library/Tabs.vue";
 import Tab from "../library/Tab.vue";
 import { ref } from 'vue';
+import ComponentDemo from "./ComponentDemo.vue";
 export default {
-    components: {Tabs, Tab},
+    components: {Tabs, Tab, ComponentDemo},
     setup(){
         const defaultTab = ref("Tab1");
+        const tabsCode = `<template>\r\n    <Tabs v-model:selected=\"defaultTab\">\r\n        <Tab title=\"Tab1\">My name is Jeff<\/Tab>\r\n        <Tab title=\"Tab2\">Jeff<\/Tab>\r\n        <Tab title=\"Tab3\">Hello Jeff<\/Tab>\r\n    <\/Tabs>\r\n<\/template>\r\n\r\n<script lang=\"ts\">\r\nimport Tabs from \"..\/library\/Tabs.vue\";\r\nimport Tab from \"..\/library\/Tab.vue\";\r\nimport { ref } from 'vue';\r\nexport default {\r\n    components: {Tabs, Tab},\r\n    setup(){\r\n        const defaultTab = ref(\"Tab1\");\r\n        return{\r\n            defaultTab,\r\n        }\r\n    }\r\n}\r\n<\/script>`;
         return{
-            defaultTab
+            defaultTab,
+            tabsCode,
         }
     }
 }
