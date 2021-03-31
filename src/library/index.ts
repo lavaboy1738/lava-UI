81e421539d8a667a8ad53de5d9ea25cc479c83a9
Ronnie Feng <ronnie@ronniefeng.com>
2021-03-30 20:24:10 -0400

    adding index file to export all the components from the library

---
+++ src/library/index.ts
@@ -0,0 +1,6 @@
+export {default as Switch} from "./Switch.vue";
+export {default as Button} from "./Button.vue";
+export {default as Tabs} from "./Tabs.vue";
+export {default as Tab} from "./Tab.vue";
+export {default as Dialogue} from "./Dialogue.vue";
+export {openDialogue as openDialogue} from "./openDialogue";
