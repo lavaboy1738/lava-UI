66644be9f7a30b1f5ab583a7d0fbf91a665472f1
Ronnie Feng <ronnie@ronniefeng.com>
2021-04-09 11:24:39 -0400

    fixing tabs error

--- rollup.config.js
+++ rollup.config.js
@@ -6,24 +6,32 @@ import { terser } from "rollup-plugin-terser";
 
 export default {
   input: "src/library/index.ts",
-  output: {
-    globals: {
-      vue: "Vue",
+  output: [
+    {
+      globals: {
+        vue: "Vue",
+      },
+      name: "Lava-UI",
+      file: "dist/library/lava-ui.js",
+      format: "umd",
+      plugins: [terser()],
     },
-    name: "Lava-UI",
-    file: "dist/library/lava-ui.js",
-    format: "umd",
-    plugins: [terser()],
-  },
+    {
+      name: "Lava-UI",
+      file: "dist/library/lava-ui.esm.js",
+      format: "es",
+      plugins: [terser()],
+    },
+  ],
   plugins: [
+    vue({
+      include: /\.vue$/,
+    }),
     scss({ include: /\.scss$/, sass: dartSass }),
     esbuild({
       include: /\.[jt]s$/,
       minify: process.env.NODE_ENV === "production",
       target: "es2015",
-    }),
-    vue({
-      include: /\.vue$/,
     }),
   ],
 };
--- src/library/Tabs.vue
+++ src/library/Tabs.vue
@@ -48,7 +48,7 @@ export default {
                 const {left: left2} = selectedItem.value.getBoundingClientRect();
                 const left = left2 - left1;
                 indicator.value.style.left = left + "px";
-            })
+            }, {flush: "post"})
         })
 
         const titles = defaults.map((tag)=>{
