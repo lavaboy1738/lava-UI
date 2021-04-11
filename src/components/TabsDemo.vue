3fb237f3bd01afe657fcc38c7092316cb1a01fba
Ronnie Feng <ronnie@ronniefeng.com>
2021-04-10 22:27:53 -0400

    fixing import for tabs demo

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -18,7 +18,7 @@ export default {
     components: {Tabs, Tab, ComponentDemo},
     setup(){
         const defaultTab = ref("Tab1");
-        const tabsCode = `<template>\r\n    <Tabs v-model:selected=\"defaultTab\">\r\n        <Tab title=\"Tab1\">My name is Jeff<\/Tab>\r\n        <Tab title=\"Tab2\">Jeff<\/Tab>\r\n        <Tab title=\"Tab3\">Hello Jeff<\/Tab>\r\n    <\/Tabs>\r\n<\/template>\r\n\r\n<script lang=\"ts\">\r\nimport Tabs from \"..\/library\/Tabs.vue\";\r\nimport Tab from \"..\/library\/Tab.vue\";\r\nimport { ref } from 'vue';\r\nexport default {\r\n    components: {Tabs, Tab},\r\n    setup(){\r\n        const defaultTab = ref(\"Tab1\");\r\n        return{\r\n            defaultTab,\r\n        }\r\n    }\r\n}\r\n<\/script>`;
+        const tabsCode = `<template>\r\n    <Tabs v-model:selected=\"defaultTab\">\r\n        <Tab title=\"Tab1\">My name is Jeff<\/Tab>\r\n        <Tab title=\"Tab2\">Jeff<\/Tab>\r\n        <Tab title=\"Tab3\">Hello Jeff<\/Tab>\r\n    <\/Tabs>\r\n<\/template>\r\n\r\n<script lang=\"ts\">\r\nimport {Tabs, Tab} from \"lava-ui\";\r\nimport \"lava-ui\/dist\/library\/lava-ui.css\";\r\nimport { ref } from 'vue';\r\nexport default {\r\n    components: {Tabs, Tab},\r\n    setup(){\r\n        const defaultTab = ref(\"Tab1\");\r\n        return{\r\n            defaultTab,\r\n        }\r\n    }\r\n}\r\n<\/script>`;
         return{
             defaultTab,
             tabsCode,
